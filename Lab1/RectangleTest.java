8b56ec6377d20685e7274f75e840069fa537498b
Taha Tariq <tahatariqf@gmail.com>
2026-02-12 00:25:30 -0800

    Used class object in another file

---
+++ Lab1/RectangleTest.java
@@ -0,0 +1,8 @@
+public class RectangleTest{
+	public static void main(String[] args){
+		Rectangle r1 = new Rectangle();
+		System.out.println(r1);
+		Rectangle r2= new Rectangle();
+		System.out.println(r2);
+}
+}
