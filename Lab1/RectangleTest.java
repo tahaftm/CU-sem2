5b879aa7ba656e37e7fb9210673e26b1d65bf122
Taha Tariq <tahatariqf@gmail.com>
2026-02-12 00:47:25 -0800

    Added custom instance variables

--- Lab1/RectangleTest.java
+++ Lab1/RectangleTest.java
@@ -2,12 +2,16 @@ public class RectangleTest{
 	public static void main(String[] args){
 		Rectangle r1 = new Rectangle();
 		System.out.println("r1 Specifications: ");
+		r1.length = 10;
+		r1.bredth = 100;
 		System.out.println("Length: " + (r1.length));
 		System.out.println("Bredth: " + (r1.bredth));
 		System.out.println();
 		System.out.println();
 		Rectangle r2= new Rectangle();
 		System.out.println("r2 Specifications: ");
+		r2.length = 20;
+		r2.bredth = 10;
 		System.out.println("Length: " + (r2.length));
 		System.out.println("Bredth: " + (r2.bredth));
 		System.out.println(r2);
