526ec5aa6f01aeba7d3eba1e552e15198b4326ac
Taha Tariq <tahatariqf@gmail.com>
2026-04-06 05:07:07 -0700

    Memory

--- learning oop from youtube/PrimitiveDatatype.java
+++ learning oop from youtube/PrimitiveDatatype.java
@@ -5,5 +5,13 @@ public class PrimitiveDatatype{
         float marks = 76.676F;
         double pi = 3.14153678980;
         System.out.println(phoneNumber + " " + marks + " " + pi);
+
+        int a = 5;
+        int b = a;
+
+        // In primitve datatype the variable creates a copy of the value not the link to the variable whos coppy is being made
+        System.out.println("b: "+b);
+        a = 7;
+        System.out.println("b: "+b);
     }
 }
