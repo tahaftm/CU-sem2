370859f84fcf4bc41b75d178a682b8b786170c79
Taha Tariq <tahatariqf@gmail.com>
2026-04-06 04:44:28 -0700

    Non Primitive

---
+++ learning oop from youtube/NonPrimitiveDatatype.java
@@ -0,0 +1,11 @@
+
+import java.util.Date;
+
+public class NonPrimitiveDatatype{
+    public static void main(String[] args){
+        // All the datatypes that does not have defined size is known as Non Primitive Datatype
+        // An object in Java is an instance/example of a class that represents a real-world entity and contains:
+        String newString = new String("This is a new text");
+        Date newDate = new Date();
+    }
+}
