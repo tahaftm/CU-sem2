c119d2f10f290ff7febf933da584bdafe4393ebe
Taha Tariq <tahatariqf@gmail.com>
2026-04-06 04:36:25 -0700

    Primitive

---
+++ learning oop from youtube/PrimitiveDatatype.java
@@ -0,0 +1,9 @@
+public class PrimitiveDatatype{
+    public static void main(String[] args){
+        // All the datatypes that have a defined size is known as Primitive Datatype
+        long phoneNumber = 3357227859L;
+        float marks = 76.676F;
+        double pi = 3.14153678980;
+        System.out.println(phoneNumber + " " + marks + " " + pi);
+    }
+}
