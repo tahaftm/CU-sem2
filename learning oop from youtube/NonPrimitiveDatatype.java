a36b4875e390cd5ba0b4c61f2c1e42cd07094e67
Taha Tariq <tahatariqf@gmail.com>
2026-04-06 05:09:20 -0700

    Memory

--- learning oop from youtube/NonPrimitiveDatatype.java
+++ learning oop from youtube/NonPrimitiveDatatype.java
@@ -1,4 +1,5 @@
 
+import java.awt.Point;
 import java.util.Date;
 
 public class NonPrimitiveDatatype{
@@ -7,5 +8,12 @@ public class NonPrimitiveDatatype{
         // An object in Java is an instance/example of a class that represents a real-world entity and contains:
         String newString = new String("This is a new text");
         Date newDate = new Date();
+
+
+        // In non primitve datatype the variable creates a link to the variable whos coppy is being made
+        Point a = new Point(1,5);
+        System.out.println(a);
+        a.x = 3;
+        System.out.println(a);
     }
 }
