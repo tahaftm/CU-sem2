9d95a8821c0e35d84068c70f00e8c90e056a475c
Taha Tariq <tahatariqf@gmail.com>
2026-05-01 10:08:56 -0700

    use of instance of

--- yt lectures/Inheritance/Demo.java
+++ yt lectures/Inheritance/Demo.java
@@ -8,8 +8,16 @@ public class Demo{
         // child_through_parent.hello();           // When child is created with the datatype of its parent
 
 
+        // A child_through_parent = new Asub();
+        // A.hello_static();
+        // Asub.hello_static();
+
         A child_through_parent = new Asub();
-        A.hello_static();
-        Asub.hello_static();
+        if (child_through_parent instanceof A) {
+            System.out.println("it is instance of A");
+        }
+        if (child_through_parent instanceof Asub) {
+            System.out.println("it is instance of Asub");
+        }
     }
 }
