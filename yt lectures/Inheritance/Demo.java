fb7597dc2aea7c454ee8a0ac1a6e78faf2bd2250
Taha Tariq <tahatariqf@gmail.com>
2026-05-01 09:28:57 -0700

    child created with the datatype of its parent

--- yt lectures/Inheritance/Demo.java
+++ yt lectures/Inheritance/Demo.java
@@ -4,6 +4,7 @@ public class Demo{
 
         // child.hello();               // Basic Concepts
 
-
+        A child_through_parent = new Asub();
+        child_through_parent.hello();           // When child is created with the datatype of its parent
     }
 }
