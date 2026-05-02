b059ce20291304b1fba1b916fab8ae64fef040fb
Taha Tariq <tahatariqf@gmail.com>
2026-05-02 04:32:40 -0700

    Function called

--- yt lectures/Inner classes/Demo.java
+++ yt lectures/Inner classes/Demo.java
@@ -1,10 +1,17 @@
 public class Demo{
     public static void main(String[] args){
         // Creating objects of inner and outer classes:
-        Nested out = new Nested();
-        Nested.Inside in = out.new Inside();
+    //     Nested out = new Nested();
+    //     Nested.Inside in = out.new Inside();
 
-        System.out.println(out.msg_out);
-        System.out.println(in.msg_in);
+    //     System.out.println(out.msg_out);
+    //     System.out.println(in.msg_in);
+    // }
+
+    // Working with functions
+            Nested out = new Nested();
+            Nested.Inside in = out.new Inside();
+            out.greetings();
+            in.greetings();
     }
 }
--- yt lectures/Inner classes/Nested.java
+++ yt lectures/Inner classes/Nested.java
@@ -1,6 +1,12 @@
 public class Nested{
     String msg_out = "I am outside";
+    public void greetings(){
+        System.out.println("Hello from outside");
+    }
     public class Inside {
         String msg_in = "I am inside";
+    public void greetings(){
+        System.out.println("Hello from inside");
+    }
     }
 }
